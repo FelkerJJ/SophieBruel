858f2a250a83f101887bb080cb586690185e8ebb
Felker Jiji <Afelker@hotmail.fr>
2024-04-15 16:21:50 +0200

    Start the login code

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -61,7 +61,14 @@
                 if (work.categoryId == categoryID) {
                     console.log(work.categoryId)
                 }
+                
+                const mySet = new Set();
 
+                mySet.add(1);
+                mySet.add(5); 
+                mySet.add(5); 
+
+                console.log(mySet);
             })
         })
     }
