5741af21ba0f4ce1d0693c78d344e4e3543928ba
Felker Jiji <Afelker@hotmail.fr>
2024-04-22 19:52:56 +0200

    Filters + img done

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -15,23 +15,42 @@ async function getData() {
     }
 }
 
-function handleFilterClick(event) {
+// Fonction pour gérer le clic sur les boutons de filtre
+async function handleFilterClick(event) {
     const categoryID = event.target.dataset.categoryId; 
-    GalleryCategory(categoryID); 
+    if (categoryID === "all") {
+        const allWorks = await getData();
+        updateGallery(allWorks);
+    } else {
+        try {
+            const filteredWorks = await filterdata(categoryID);
+            updateGallery(filteredWorks);
+        } catch (error) {
+            console.error("Erreur lors de la mise à jour de la galerie:", error);
+        }
+    }
 }
 
-function GalleryCategory(categoryID) {
-    filterdata(categoryID);
-    const galleryItems = document.querySelectorAll('.gallery figure');
-    galleryItems.forEach(item => {
-        const shouldDisplay = (item.classList.contains(categoryID) || categoryID);
-        item.style.display = shouldDisplay ? 'block' : 'none';
+// Mise à jour de la galerie avec les données filtrées
+function updateGallery(filteredWorks) {
+    const gallery = document.querySelector('.gallery');
+    gallery.innerHTML = ''; // Efface les images précédentes de la galerie
+    filteredWorks.forEach(work => {
+        const figure = document.createElement('figure');
+        const img = document.createElement('img');
+        img.src = work.imageUrl;
+        img.alt = work.title;
+        figure.appendChild(img);
+        const figcaption = document.createElement('figcaption');
+        figcaption.textContent = work.title;
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
     });
 }
 
+// Récupération des données et configuration des écouteurs d'événements pour les boutons de filtre
 getData().then(data => {
     try {
-
         const filterButtons = document.querySelectorAll(".filters-button button");
         filterButtons.forEach(button => {
             button.addEventListener('click', handleFilterClick);
@@ -41,13 +60,8 @@ getData().then(data => {
     }
 });
 
-function filterdata(categoryID) {
-    getData().then(works => {
-        works.forEach(work => {
-            if (work.categoryId == categoryID) {
-                console.log(work.imageUrl)
-                console.log(work.title)
-            }
-        })
-    })
+// Fonction pour filtrer les données en fonction de la catégorie sélectionnée
+async function filterdata(categoryID) {
+    const works = await getData();
+    return works.filter(work => work.categoryId == categoryID);
 }
