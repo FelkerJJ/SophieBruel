bf30e48a137a44001b8aa8d385383425ff584eb5
Felker Jiji <Afelker@hotmail.fr>
2024-05-20 19:00:46 +0200

    First and second modal OK + clean & refacto

--- FrontEnd/assets/secondModal.js
+++ FrontEnd/assets/secondModal.js
@@ -5,15 +5,34 @@ document.getElementById("addPhotoButton").addEventListener("click", function() {
     document.getElementById("addPhotoForm").style.display = "block";
     document.getElementById("pictureBloc").style.display = "block";
     document.getElementById("previousButtonModal2").classList.remove("hidden");
-    document.querySelector(".modal-content .arrow-left .fa-solid.fa-arrow-left").style.display = "block";
+    document.querySelector(".mainContainer").style.bottom = "22px";
+    document.querySelector(".mainContainer").style.position = "relative";
+    document.querySelector(".close").style.top = "0px";
+    arrowLeftIcon.style.display = "block"; 
 });
 
-    
-// Modifs ajoutés by le bouton "Ajouter une photo"
+
+
+// Ajouter un gestionnaire d'événements au clic pour masquer l'icône de la flèche gauche
+arrowLeftIcon.addEventListener("click", function() {
+    document.getElementById("modalTitle1").textContent = "Galerie photo";
+    document.querySelector(".modal-gallery").style.display = "grid";
+    document.getElementById("addPhotoForm").style.display = "none";
+    document.getElementById("pictureBloc").style.display = "none";
+    customCTAButton.textContent = "Ajouter une photo"; 
+    customCTAButton.style.display = "block"; 
+    customCTAButton.classList.remove("elementCTA", "elementLign"); 
+    document.querySelector(".ligne-modal").classList.remove("elementLign");
+    document.querySelector(".ligne-modal").classList.add("ligne-modalreturn"); 
+});
+
+
+
+// Modifs ajoutées par le bouton "Ajouter une photo"
 const customCTAButton = document.getElementById("addPhotoButton");
-    customCTAButton.addEventListener("click", function() {
-        customCTAButton.textContent = "Valider"; 
-        customCTAButton.classList.add("elementCTA"); 
-        customCTAButton.classList.add("elementLign"); 
-        document.querySelector(".ligne-modal").classList.add("elementLign");
+customCTAButton.addEventListener("click", function() {
+    customCTAButton.textContent = "Valider"; 
+    customCTAButton.classList.add("elementCTA"); 
+    customCTAButton.classList.add("elementLign"); 
+    document.querySelector(".ligne-modal").classList.add("elementLign");
 });
--- FrontEnd/assets/showModal.js
+++ FrontEnd/assets/showModal.js
@@ -1,20 +1,25 @@
 // Récupérer la modale
 const modal = document.getElementById("myModal");
 const modifierBtn = document.querySelector(".modifier-text");
-const span = document.querySelector(".close");
+const closeSpan = document.querySelector(".close");
+
+arrowLeftIcon.style.display = "none";
 
 modifierBtn.onclick = function() {
   modal.style.display = "block";
 }
 
 // Quand l'utilisateur clique sur <span> (x), fermer la modale
-span.onclick = function() {
+closeSpan.onclick = function() {
   modal.style.display = "none";
+  arrowLeftIcon.style.display = "none";
 }
 
 // Quand l'utilisateur clique en dehors de la modale, la fermer
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    arrowLeftIcon.style.display = "none"; 
+
   }
 }
