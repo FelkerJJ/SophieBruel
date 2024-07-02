c1d6d4d5f00fe82a294d0452e5558d61eaf6b7c2
Felker Jiji <Afelker@hotmail.fr>
2024-07-02 14:53:22 +0200

    Refacto delete.JS done

--- FrontEnd/assets/deleteData/delete.js
+++ FrontEnd/assets/deleteData/delete.js
@@ -18,30 +18,14 @@ async function deleteImage(workId) {
         // Supprimer l'image de la modale
         const imageContainer = document.querySelector(`[data-item-id="${workId}"]`);
         if (imageContainer) {
-            imageContainer.parentElement.removeChild(imageContainer);
         } else {
             console.error('Image container not found in modal');
         }
 
-        // Mettre à jour les galeries pour refléter la suppression
+        // Mettre à jour les galeries pour refléter avec la suppression
         await fetchDataAndUpdateGalleries();
     } else {
         console.error('Failed to delete image');
-    }
-}
-
-// Gestionnaire d'événements pour soumettre le formulaire d'ajout d'image
-async function handleImageUpload(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-
-    try {
-        const imageData = await uploadNewImage(formData);
-        addNewWork(imageData);
-        event.target.reset();
-    } catch (error) {
-        console.error('Erreur lors du traitement de l\'image:', error);
     }
 }
 
